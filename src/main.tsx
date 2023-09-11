6bcad234c3d0d6e315233f44c2e5abdc973ec193
jvillegasl <jvillegasl@uni.pe>
2023-09-11 18:50:13 -0500

    styles and toastify implemented

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,36 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import "./index.scss";
+import "react-toastify/dist/ReactToastify.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { AuthProvider } from "react-auth-kit";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { ToastContainer } from "react-toastify";
+
+const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <AuthProvider authType="localstorage" authName="_token">
-            <RouterProvider router={router} />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={true}
+                    theme="colored"
+                    pauseOnHover
+                    pauseOnFocusLoss
+                    closeOnClick
+                />
+                <RouterProvider router={router} />
+            </ThemeProvider>
         </AuthProvider>
     </React.StrictMode>,
 );
