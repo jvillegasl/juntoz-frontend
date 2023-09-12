255b193d544a3a6666fb49c8733cc1990df3a830
jvillegasl <jvillegasl@uni.pe>
2023-09-12 12:03:41 -0500

    WIP: root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import { useQuery } from "react-query";
+
+import { getOrders } from "@/services";
+import { OrdersTable } from "./OrdersTable";
+
 export default function Root() {
-    return <h1>This is the root route</h1>;
+    const { data: orders, isLoading, error } = useQuery("orders", getOrders);
+
+    if (isLoading || !orders) return <h1>Loading</h1>;
+
+    if (error) return <h1>Fetching failed</h1>;
+
+    return <OrdersTable orders={orders} />;
 }
