4eb323d752e3532689eb8d8ab9edc5911b3a6021
jvillegasl <jvillegasl@uni.pe>
2023-09-12 14:28:15 -0500

    logout button implemented

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,11 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import { useMemo } from "react";
+import { useSignOut } from "react-auth-kit";
 import { Outlet, useLocation } from "react-router-dom";
 
 export default function RootLayout() {
     const { pathname } = useLocation();
+    const signOut = useSignOut();
 
     const title = useMemo(() => {
         if (pathname.startsWith("/details")) return "Order Details";
@@ -11,15 +13,37 @@ export default function RootLayout() {
         return "Orders";
     }, [pathname]);
 
+    function handleLogout() {
+        signOut();
+    }
+
     return (
         <Container maxWidth="lg" sx={{ p: 4 }}>
             <Box component="header" sx={{ mb: 3 }}>
-                <Typography component="h1" variant="h4">
-                    {title}
-                </Typography>
+                <Grid
+                    container
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Typography component="h1" variant="h4">
+                            {title}
+                        </Typography>
+                    </Grid>
+
+                    <Grid item>
+                        <Button
+                            color="warning"
+                            variant="outlined"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </Button>
+                    </Grid>
+                </Grid>
             </Box>
 
-            <Box component="main" mx={{sm: 4}}> 
+            <Box component="main" mx={{ sm: 4 }}>
                 <Outlet />
             </Box>
         </Container>
